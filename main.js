c13d3955a566407f5ccaf4bbc028c1b308b20ecf
shivam2399 <dubey.shivam2399@gmail.com>
2022-12-26 10:13:07 +0530

    simultaneous requests

--- main.js
+++ main.js
@@ -47,8 +47,15 @@ function removeTodo() {
 
 // SIMULTANEOUS DATA
 function getData() {
-  console.log('Simultaneous Request');
+  axios
+    .all([
+    axios.get('https:jsonplaceholder.typicode.com/todos?_limit=5'),
+    axios.get('https:jsonplaceholder.typicode.com/posts?_limit=5')
+  ])
+    .then(axios.spread((todos, posts) => showOutput(posts)))
+    .catch(err => console.error(err));
 }
+
 
 // CUSTOM HEADERS
 function customHeaders() {
