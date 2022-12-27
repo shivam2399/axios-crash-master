faabd15cd3892cdb7f560f16c623d26b44bac6de
shivam2399 <dubey.shivam2399@gmail.com>
2022-12-27 09:26:45 +0530

    create instances

--- main.js
+++ main.js
@@ -153,6 +153,11 @@ axios.interceptors.request.use(config => {
 })
 
 // AXIOS INSTANCES
+const axiosInstance = axios.create({
+  baseURL: 'https://jsonplaceholder.typicode.com'
+});
+
+// axiosInstance.get('/comments').then(res => showOutput(res));
 
 // Show output in browser
 function showOutput(res) {
